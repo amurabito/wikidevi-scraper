c5d3d0f2891eb3b2f73189f072406c00a3eae7b3
admo <anthony.murabito+gitlab@gmail.com>
2016-09-11 08:54:54 -0700

    reverting wikidevi

--- wikidevi.py
+++ wikidevi.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python
 # wikidevi.py
-<<<<<<< HEAD
 # v.52 - Poland Springs
-=======
-# v.51 - Poland Springs
->>>>>>> b7374cb5916daa4237d660910112eb21604798f0
 
 import simplemediawiki
 from sys import argv
 
-<<<<<<< HEAD
 
-=======
->>>>>>> b7374cb5916daa4237d660910112eb21604798f0
 def main():
 
     wiki = init()
@@ -21,17 +14,11 @@ def main():
     if chosenAP:
         formatAndPrint(chosenAP)
 
-<<<<<<< HEAD
 
 def init():
     wiki = simplemediawiki.MediaWiki('https://wikidevi.com/w/api.php')
     simplemediawiki.build_user_agent(
         "wikidevi-scraper", ".51", "gitlab.com/admo")
-=======
-def init():
-    wiki = simplemediawiki.MediaWiki('https://wikidevi.com/w/api.php')
-    simplemediawiki.build_user_agent("wikidevi-scraper", ".51", "gitlab.com/admo")
->>>>>>> b7374cb5916daa4237d660910112eb21604798f0
     return wiki
 
 
@@ -68,11 +55,7 @@ def searchForAP(wiki):
         i = 1
         searchResults = list()
         for item in results.get('search'):
-<<<<<<< HEAD
             if "REDIRECT" not in item['snippet']:
-=======
-            if not "REDIRECT" in item['snippet']:
->>>>>>> b7374cb5916daa4237d660910112eb21604798f0
                 print str(i) + ":\t" + item['title']
                 i += 1
                 searchResults.append(item)
@@ -94,40 +77,25 @@ def querySpecificPage(wiki, model):
                   'rvprop': 'content',
                   'format': 'json'}
         response = wiki.call(values)
-<<<<<<< HEAD
         # print response
         return response
     return False
 
 
-=======
-        #print response
-        return response
-    return False
-
->>>>>>> b7374cb5916daa4237d660910112eb21604798f0
 def formatAndPrint(model):
     accessPoint = model['query']['pages']
     temp = str(accessPoint).partition("{{")[2].rpartition("}}")[0].split("\\n")
 
     # full details, TODO - key off of DEBUG enabled
-<<<<<<< HEAD
     # for property in temp:
-=======
-    #for property in temp:
->>>>>>> b7374cb5916daa4237d660910112eb21604798f0
     #    print property
 
     # print a summary of key metrics
 
-<<<<<<< HEAD
     keyMetrics = ["|brand", "|model", "revision", "country", "|type",
                   "fcc_id", "cpu1_brand", "cpu1_model", "ram1_brand",
                   "ram1_model", "wi1", "wi2", "lan", "802dot11",
                   "default", "oui"]
-=======
-    keyMetrics = ["|brand", "|model", "revision", "country", "|type", "fcc_id", "cpu1_brand", "cpu1_model", "ram1_brand", "ram1_model", "wi1", "wi2", "lan", "802dot11", "default", "oui"]
->>>>>>> b7374cb5916daa4237d660910112eb21604798f0
 
     print "Access Point Summary:"
     for item in temp:
